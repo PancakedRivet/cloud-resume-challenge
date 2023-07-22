1ca318c5b1820a76c41d58386444c6963938e713
Patrick <85083156+PancakedRivet@users.noreply.github.com>
2023-07-23 11:56:18 +1200

    Adding a Ping to check GitHub action

--- backend/function_app.py
+++ backend/function_app.py
@@ -4,7 +4,31 @@ import logging
 from table_helper import TableServiceHelper
 
 app = func.FunctionApp()
-    
+
+@app.route(route="ping")
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Ping recieved')
+
+    name = req.params.get('name') 
+    if not name: 
+        try: 
+            req_body = req.get_json() 
+        except ValueError: 
+            pass 
+        else: 
+            name = req_body.get('name') 
+            
+    if name: 
+        return func.HttpResponse( 
+            f"Hello, {name}. Pong",
+            status_code=200
+        )
+        
+    return func.HttpResponse(
+        "Pong",
+        status_code=200
+    )
+
 @app.route(route="count", auth_level=func.AuthLevel.ANONYMOUS)
 def GetVisitorCount(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Getting visitor count from CosmosDB')
