f227b248ab3c43b82a1ca0d69a0334a3eaa57451
Patrick <85083156+PancakedRivet@users.noreply.github.com>
2023-07-22 20:41:46 +1200

    Azure Func now tested with Table API

--- backend/function_app.py
+++ backend/function_app.py
@@ -1,25 +1,35 @@
 import azure.functions as func
 import logging
 
+from table_helper import TableServiceHelper
+
 app = func.FunctionApp()
+    
+@app.route(route="count", auth_level=func.AuthLevel.ANONYMOUS)
+def GetVisitorCount(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Getting visitor count from CosmosDB')
+    table_service_helper = TableServiceHelper()
+    count = table_service_helper.get_visitor_count()
+    return func.HttpResponse(
+        "{}".format(count),
+        status_code=200
+    )
 
-@app.route(route="HttpTrigger", auth_level=func.AuthLevel.ANONYMOUS)
-def HttpTrigger(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+@app.route(route="increment", auth_level=func.AuthLevel.ANONYMOUS)
+def IncrementVisitorCount(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Incrememnting visitor count in CosmosDB')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
+    table_service_helper = TableServiceHelper()
+    count = table_service_helper.get_visitor_count()
+    try:
+        table_service_helper.upsert_visitor_count(count + 1)
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+            "{}".format(count + 1),
+            status_code=200
+        )
+    except Exception as error:
+        logging.error("An error occurred incrementing visitor count. Error = {}".format(error))
+        return func.HttpResponse(
+            "An error occurred incrementing visitor count. Error = {}".format(error),
+            status_code=500
         )
--- backend/table_helper.py
+++ backend/table_helper.py
@@ -1,10 +1,8 @@
-import json
 import os
-
 from azure.data.tables import TableServiceClient
 from dotenv import load_dotenv
 
-load_dotenv(verbose=True)
+load_dotenv()
 
 class TableServiceHelper:
 
@@ -13,4 +11,22 @@ class TableServiceHelper:
         self.conn_str = conn_str if conn_str else os.getenv("conn_str")
         self.table_service = TableServiceClient.from_connection_string(self.conn_str)
         self.table_client = self.table_service.get_table_client(self.table_name)
-
+    
+    def upsert_visitor_count(self, count = 1):
+        params = {}
+        params["PartitionKey"] = "statistic"
+        params["RowKey"] = "visitor_count"
+        params["count"] = count
+        return self.table_client.upsert_entity(params)
+    
+    def get_visitor_count(self):
+        result = self.table_client.get_entity("statistic", "visitor_count")
+        return result["count"]
+    
+if __name__ == "__main__":
+    table_service_helper = TableServiceHelper()
+    count = table_service_helper.get_visitor_count()
+    print(count)
+    table_service_helper.upsert_visitor_count(count + 1)
+    new_count = table_service_helper.get_visitor_count()
+    print(new_count)
